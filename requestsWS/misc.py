b10437a6a46bb3bd7c8fed55849b011484878f70
jona799t <jonathan.b.skettrup@gmail.com>
2022-02-09 13:40:19 +0100

    Fixed bug that occures when no encryption is specified

--- requestsWS/misc.py
+++ requestsWS/misc.py
@@ -3,10 +3,11 @@ import zlib
 
 _zlib = zlib.decompressobj()
 def decompress(message, encryption):
-    if encryption.lower() == "zlib":
-        return _zlib.decompress(message).decode("utf-8")
-    elif encryption.lower() == "gzip":
-        return str(gzip.decompress(message), encoding="utf8")
+    if type(encryption) == str:
+        if encryption.lower() == "zlib":
+            return _zlib.decompress(message).decode("utf-8")
+        elif encryption.lower() == "gzip":
+            return str(gzip.decompress(message), encoding="utf8")
     elif encryption == None:
         return message
     else:
